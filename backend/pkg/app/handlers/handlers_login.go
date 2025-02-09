cb32776f4a5c33229b5cd3e9eb7cdc7ce7ef31e1
yelmach <yassineelmach0@gmail.com>
2025-02-09 23:15:27 +0100

    add registration handler: validate request & add user to db

--- backend/pkg/app/handlers/handlers_login.go
+++ backend/pkg/app/handlers/handlers_login.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"html"
 	"net/http"
 	"strconv"
 	"strings"
@@ -10,6 +11,7 @@ import (
 	"blank/pkg/app/middleware"
 	"blank/pkg/app/models"
 	"blank/pkg/app/services"
+	"blank/pkg/app/utils"
 
 	"github.com/gofrs/uuid/v5"
 )
@@ -21,52 +23,70 @@ type AuthHandler struct {
 	MessageHandler *MessageHandler
 }
 
-func (h *AuthHandler) RegisterHandle(w http.ResponseWriter, r *http.Request) {
-	ActiveUser, _ := h.AuthMidlaware.IsUserLoggedIn(w, r)
-
-	if r.Method == http.MethodPost {
-		if ActiveUser {
-			sendResponse(w, "session")
-			return
-		}
-		var info models.SignUpData
-		err := json.NewDecoder(r.Body).Decode(&info)
-		defer r.Body.Close()
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if !h.AuthMidlaware.IsValidGender(info.Gender) ||
-			!h.AuthMidlaware.IsValidAge(info.Age) ||
-			!h.AuthMidlaware.IsValidEmail(info.Email) ||
-			!h.AuthMidlaware.IsValidName(info.Username) ||
-			!h.AuthMidlaware.IsValidPassword(info.Passwd) ||
-			!h.AuthMidlaware.IsmatchPassword(info.Passwd, info.ConfirmPasswd) {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = h.AuthService.Register(info)
-		if err != nil {
-			switch true {
-			case strings.Contains(err.Error(), "username"):
-				sendResponse(w, "user")
-				return
-			case err.Error() == "email":
-				sendResponse(w, "email")
-				return
-			default:
-				sendResponse(w, "passwd")
-				return
-			}
-		}
-		sendResponse(w, "Done")
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+func (h *AuthHandler) HandleRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.SendResponses(w, http.StatusMethodNotAllowed, "method not allowed", nil)
 		return
 	}
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		utils.SendResponses(w, http.StatusUnsupportedMediaType, "content-Type must be application/json", nil)
+		return
+	}
+
+	var user models.RegisterData
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		utils.SendResponses(w, http.StatusBadRequest, "invalid JSON data", nil)
+		return
+	}
+
+	user.FirstName = html.EscapeString(strings.TrimSpace(user.FirstName))
+	user.LastName = html.EscapeString(strings.TrimSpace(user.LastName))
+	user.Email = html.EscapeString(strings.TrimSpace(user.Email))
+	user.Nickname = html.EscapeString(strings.TrimSpace(user.Nickname))
+	user.DateOfBirth = html.EscapeString(strings.TrimSpace(user.DateOfBirth))
+	user.AboutMe = html.EscapeString(strings.TrimSpace(user.AboutMe))
+
+	if isValid, message := h.AuthMidlaware.ValidateFullName(user.FirstName); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidateFullName(user.LastName); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidateDateOfBirth(user.DateOfBirth); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidateNickname(user.Nickname); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidateAboutMe(user.AboutMe); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidateEmail(user.Email); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	if isValid, message := h.AuthMidlaware.ValidatePassword(user.Password); !isValid {
+		utils.SendResponses(w, http.StatusBadRequest, message, nil)
+		return
+	}
+
+	status, message := h.AuthService.Register(user)
+	utils.SendResponses(w, status, message, nil)
 }
 
-func (h *AuthHandler) LoginHandle(w http.ResponseWriter, r *http.Request) {
+func (h *AuthHandler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		var info models.LoginData
 		err := json.NewDecoder(r.Body).Decode(&info)
@@ -79,10 +99,10 @@ func (h *AuthHandler) LoginHandle(w http.ResponseWriter, r *http.Request) {
 		if err != nil || user == nil {
 			switch true {
 			case err.Error() == "in email or username":
-				sendResponse(w, "Account Not found")
+				// sendResponse(w, "Account Not found")
 				return
 			case strings.Contains(err.Error(), "password"):
-				sendResponse(w, "passwd")
+				// sendResponse(w, "passwd")
 				return
 			}
 		}
@@ -94,26 +114,9 @@ func (h *AuthHandler) LoginHandle(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		SetCookies(w, "sessionId", sessionId, sessionExpires)
-		sendResponse(w, "Done")
+		// sendResponse(w, "Done")
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
-func sendResponse(w http.ResponseWriter, reply string) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if reply != "Done" {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-
-	response := &models.LoginReply{
-		REplyMssg: reply,
-	}
-
-	err := json.NewEncoder(w).Encode(&response)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
@@ -129,7 +132,7 @@ func SetCookies(w http.ResponseWriter, name, value string, expires time.Time) {
 	http.SetCookie(w, cookie)
 }
 
-func (h *AuthHandler) LogoutHandle(w http.ResponseWriter, r *http.Request) {
+func (h *AuthHandler) HandleLogout(w http.ResponseWriter, r *http.Request) {
 	activeUser, user := h.AuthMidlaware.IsUserLoggedIn(w, r)
 	if !activeUser {
 		w.WriteHeader(http.StatusBadRequest)
@@ -145,7 +148,7 @@ func (h *AuthHandler) LogoutHandle(w http.ResponseWriter, r *http.Request) {
 	}
 	h.MessageHandler.DisconnectClient(user.ID.String())
 	SetCookies(w, "sessionId", "", time.Now().Add(-1*time.Hour))
-	sendResponse(w, "Done")
+	// sendResponse(w, "Done")
 }
 
 func (h *AuthHandler) UserIntegrity(w http.ResponseWriter, r *http.Request) {
@@ -160,9 +163,9 @@ func (h *AuthHandler) UserIntegrity(w http.ResponseWriter, r *http.Request) {
 	}
 	exist := h.SessionService.CheckSession(sessionId.Value)
 	if !exist {
-		sendResponse(w, "No User Found")
+		// sendResponse(w, "No User Found")
 	} else {
-		sendResponse(w, "Done")
+		// sendResponse(w, "Done")
 	}
 }
 
