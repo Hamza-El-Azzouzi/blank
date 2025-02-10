c04492a8c21d4de1aa7cbfb7c12270a759815e27
Hamza Maach <hamzamaach56@gmail.com>
2025-02-10 17:34:12 +0100

    add allowed methods & headers to CORS middleware

--- backend/pkg/app/middleware/cors.go
+++ backend/pkg/app/middleware/cors.go
@@ -4,14 +4,21 @@ import (
 	"net/http"
 )
 
-
 func CheckCORS(next http.Handler) http.Handler {
-	var originAllowlist = "http://127.0.0.1:3000"
+	originAllowlist := "http://127.0.0.1:3000"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		if origin == originAllowlist {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		}
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
