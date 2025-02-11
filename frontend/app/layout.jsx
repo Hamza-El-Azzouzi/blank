652b85eed8fdbbaf4c7c303e85503e3566900964
Hamza Maach <hamzamaach56@gmail.com>
2025-02-11 11:42:54 +0100

    add favicon and update toast position

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -13,12 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main className="container mx-auto px-4 pt-16">
           {children}
         </main>
-        <Toaster />
+        <Toaster position="top-right"/>
       </body>
     </html>
   );
