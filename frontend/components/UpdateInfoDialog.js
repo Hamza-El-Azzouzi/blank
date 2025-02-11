4eed3da26f183350808920c713b720ec6c4cf622
Hamza Maach <hamzamaach56@gmail.com>
2025-02-11 15:14:46 +0100

    add names to UpdateInfoDialog's inputs

--- frontend/components/UpdateInfoDialog.js
+++ frontend/components/UpdateInfoDialog.js
@@ -85,32 +85,32 @@ export default function UpdateInfoDialog({ user, onClose, setProfile }) {
         <form onSubmit={handleSubmit} className="space-y-2">
           <div>
             <label htmlFor="first_name" className="block text-sm text-gray-500 mb-1 ml-2">First Name</label>
-            <Input id="first_name" value={formData.first_name} onChange={handleChange} placeholder="First Name" />
+            <Input id="first_name" name="first_name" value={formData.first_name} onChange={handleChange} placeholder="First Name" />
           </div>
 
           <div>
             <label htmlFor="last_name" className="block text-sm text-gray-500 mb-1 ml-2">Last Name</label>
-            <Input id="last_name" value={formData.last_name} onChange={handleChange} placeholder="Last Name" />
+            <Input id="last_name" name="last_name" value={formData.last_name} onChange={handleChange} placeholder="Last Name" />
           </div>
 
           <div>
             <label htmlFor="nickname" className="block text-sm text-gray-500 mb-1 ml-2">Nickname</label>
-            <Input id="nickname" value={formData.nickname} onChange={handleChange} placeholder="Nickname" />
+            <Input id="nickname" name="nickname" value={formData.nickname} onChange={handleChange} placeholder="Nickname" />
           </div>
 
           <div>
             <label htmlFor="email" className="block text-sm text-gray-500 mb-1 ml-2">Email</label>
-            <Input id="email" type="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+            <Input id="email" name="email" type="email" value={formData.email} onChange={handleChange} placeholder="Email" />
           </div>
 
           <div>
             <label htmlFor="about" className="block text-sm text-gray-500 mb-1 ml-2">About You</label>
-            <Textarea className="resize-none" id="about" value={formData.about} onChange={handleChange} placeholder="About You" />
+            <Textarea className="resize-none" name="about" id="about" value={formData.about} onChange={handleChange} placeholder="About You" />
           </div>
 
           <div>
             <label htmlFor="date_of_birth" className="block text-sm text-gray-500 mb-1 ml-2">Date of Birth</label>
-            <Input id="date_of_birth" type="date" value={formData.date_of_birth?.split('T')[0]} onChange={handleChange} />
+            <Input id="date_of_birth" name="date_of_birth" type="date" value={formData.date_of_birth?.split('T')[0]} onChange={handleChange} />
           </div>
 
           <div className="">
