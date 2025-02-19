2941145b5bb6643a57226a11751e25eb1697d533
hkhlifi <khllifihamza66@gmail.com>
2025-02-19 13:02:48 +0100

    init follow system

--- backend/pkg/app/handlers/handlers_follow.go
+++ backend/pkg/app/handlers/handlers_follow.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"blank/pkg/app/models"
 	"blank/pkg/app/services"
+	"blank/pkg/app/utils"
 )
 
 type FollowHandler struct {
@@ -11,12 +14,51 @@ type FollowHandler struct {
 }
 
 func (f *FollowHandler) RequestFollow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.SendResponses(w, http.StatusMethodNotAllowed, "Method Not Allowed", nil)
+		return
+	}
+
+	var follow models.FollowRequest
+
+	err := json.NewDecoder(r.Body).Decode(&follow)
+	if err != nil {
+		utils.SendResponses(w, http.StatusBadRequest, "Bad request", nil)
+		return
+	}
+	defer r.Body.Close()
 }
 
 func (f *FollowHandler) AcceptFollow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.SendResponses(w, http.StatusMethodNotAllowed, "Method Not Allowed", nil)
+		return
+	}
+
+	var follow models.FollowRequest
+
+	err := json.NewDecoder(r.Body).Decode(&follow)
+	if err != nil {
+		utils.SendResponses(w, http.StatusBadRequest, "Bad request", nil)
+		return
+	}
+	defer r.Body.Close()
 }
 
 func (f *FollowHandler) RefuseFollow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.SendResponses(w, http.StatusMethodNotAllowed, "Method Not Allowed", nil)
+		return
+	}
+
+	var follow models.FollowRequest
+
+	err := json.NewDecoder(r.Body).Decode(&follow)
+	if err != nil {
+		utils.SendResponses(w, http.StatusBadRequest, "Bad request", nil)
+		return
+	}
+	defer r.Body.Close()
 }
 
 func (f *FollowHandler) DeleteFollowing(w http.ResponseWriter, r *http.Request) {
