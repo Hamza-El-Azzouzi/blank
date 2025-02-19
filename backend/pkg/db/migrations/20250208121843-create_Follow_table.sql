38a8ddac774415a6b17a8c3612ba16557e8ae101
hkhlifi <khllifihamza66@gmail.com>
2025-02-19 12:33:07 +0100

    init follow system

--- backend/pkg/db/migrations/20250208121843-create_Follow_table.sql
+++ backend/pkg/db/migrations/20250208121843-create_Follow_table.sql
@@ -2,15 +2,15 @@
 -- +migrate Up
 CREATE TABLE `Follow` (
   `follower_id` text,
-  `followed_id` text,
+  `following_id` text,
   `status` varchar(255),
-  PRIMARY KEY (`follower_id`, `followed_id`),
+  PRIMARY KEY (`follower_id`, `following_id`),
   FOREIGN KEY (`follower_id`) REFERENCES `User` (`user_id`),
-  FOREIGN KEY (`followed_id`) REFERENCES `User` (`user_id`)
+  FOREIGN KEY (`following_id`) REFERENCES `User` (`user_id`)
 );
 
 CREATE INDEX idx_follow_follower_id ON `Follow` (`follower_id`);
-CREATE INDEX idx_follow_followed_id ON `Follow` (`followed_id`);
+CREATE INDEX idx_follow_following_id ON `Follow` (`following_id`);
 CREATE INDEX idx_follow_status ON `Follow` (`status`);
 
 -- +migrate Down
