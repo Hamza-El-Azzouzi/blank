bdff9adee04782759fc4d673954ce1795999eb6b
Hamza Maach <hamzamaach56@gmail.com>
2025-02-27 13:11:24 +0000

    allow user to have multy connections

--- backend/pkg/app/services/service_ws.go
+++ backend/pkg/app/services/service_ws.go
@@ -2,19 +2,24 @@ package services
 
 import (
 	"fmt"
+	"log"
+	"sync"
 
 	"blank/pkg/app/models"
 	"blank/pkg/app/repositories"
 
 	"github.com/gofrs/uuid/v5"
+	"github.com/gorilla/websocket"
 )
 
 type WebSocketService struct {
-	UserRepo    *repositories.UserRepository
-	MessageRepo *repositories.MessageRepository
+	UserRepo       *repositories.UserRepository
+	MessageRepo    *repositories.MessageRepository
+	ConnectedUsers map[uuid.UUID]*models.ConnectedUser
+	Mutex          sync.Mutex
 }
 
-func (ws *WebSocketService) NotificationService(message models.Message) ([]uuid.UUID, models.Notification) {
+func (ws *WebSocketService) ReadMessage(message models.Message) ([]uuid.UUID, models.Notification) {
 	var (
 		notification models.Notification
 		dists        []uuid.UUID // distinations of the notification
@@ -89,4 +94,62 @@ func (ws *WebSocketService) SendMessageToGroup(sender *models.UserInfo, message 
 	)
 
 	return dists, notification, nil
+}
+
+func (ws *WebSocketService) SendNotification(dists []uuid.UUID, notification models.Notification) error {
+	ws.Mutex.Lock()
+	defer ws.Mutex.Unlock()
+
+	for _, dist := range dists {
+		if user, ok := ws.ConnectedUsers[dist]; ok {
+			for _, conn := range user.Connections {
+				err := conn.WriteJSON(notification)
+				if err != nil {
+					log.Printf("Error sending notification to %s: %v", user.User.FirstName, err)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (ws *WebSocketService) ConnectUser(conn *websocket.Conn, userID uuid.UUID) error {
+	ws.Mutex.Lock()
+	defer ws.Mutex.Unlock()
+
+	// Check if the user already has connections
+	if user, exists := ws.ConnectedUsers[userID]; exists {
+		user.Connections = append(user.Connections, conn)
+	} else {
+		user, err := ws.UserRepo.GetPublicUserInfo(userID)
+		if err != nil {
+			return err
+		}
+		ws.ConnectedUsers[userID] = &models.ConnectedUser{
+			Connections: []*websocket.Conn{conn},
+			User:        user,
+		}
+		log.Printf("%s %s connected\n", user.FirstName, user.LastName)
+	}
+	return nil
+}
+
+func (ws *WebSocketService) DisconnectUser(conn *websocket.Conn, userID uuid.UUID) {
+	ws.Mutex.Lock()
+	defer ws.Mutex.Unlock()
+
+	if user, exists := ws.ConnectedUsers[userID]; exists {
+		for i, c := range user.Connections {
+			if c == conn {
+				user.Connections = append(user.Connections[:i], user.Connections[i+1:]...)
+				break
+			}
+		}
+		conn.Close()
+
+		if len(user.Connections) == 0 {
+			delete(ws.ConnectedUsers, userID)
+			log.Printf("%s %s disconnected\n", user.User.FirstName, user.User.LastName)
+		}
+	}
 }
