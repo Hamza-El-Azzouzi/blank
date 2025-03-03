b9d99c454ec9384ec24bf80e9ef28a05a2b5544a
hamza-el-azzouzi <elazzouzi.hamza20@gmail.com>
2025-03-03 14:31:48 +0000

    dockerizing both sides

--- backend/script.sh
+++ backend/script.sh
@@ -1,6 +1,5 @@
 docker build -t blank-server .
+
 docker run -d --network=blank-network -p 1414:1414 --name blank-server-container blank-server
-docker container ps
-docker images
-docker logs blank-server-container
-docker system prune -a
+
+
