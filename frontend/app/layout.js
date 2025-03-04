9f83c27244bbe19836b63a9382ed562fabc853b0
hamza-el-azzouzi <elazzouzi.hamza20@gmail.com>
2025-03-04 09:42:31 +0000

    remove Provider&Context

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,8 +1,7 @@
 'use client';
 
 import "./globals.css";
-import { AuthProvider } from '../contexts/AuthContext';
-import ProtectedRoute from '../components/ProtectedRoute';
+
 
 // export const metadata = {
 //   title: "Social Network",
@@ -13,13 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          <ProtectedRoute>
-            <div className="container">
-              {children}
-            </div>
-          </ProtectedRoute>
-        </AuthProvider>
+        <div className="container">
+          {children}
+        </div>
       </body>
     </html>
   );
