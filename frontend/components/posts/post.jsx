df029f8ea816ce2ae18222416d4e00bf927d7c71
yassinalmach <yassineelmach0@gmail.com>
2025-03-09 09:53:55 +0000

    fix post privacy icon style after login

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -64,10 +64,10 @@ const Post = ({ post, target }) => {
             <span className="post-author">{post.author}</span>
           </Link>
           <span className="post-time">
-            <div style={{ fontSize: '13px' }}>
-              {post.privacy === 'public' && <FiGlobe className="privacy-icon" />}
-              {post.privacy === 'almost private' && <FiUsers className="privacy-icon" />}
-              {post.privacy === 'private' && <FiLock className="privacy-icon" />}
+            <div className='post-privacy-icon'>
+              {post.privacy === 'public' && <FiGlobe />}
+              {post.privacy === 'almost private' && <FiUsers />}
+              {post.privacy === 'private' && <FiLock />}
             </div>
             <div>{post.formatted_date}</div>
           </span>
